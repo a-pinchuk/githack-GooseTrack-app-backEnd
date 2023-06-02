c63ec24f835e623af74b02a1981c7ff8cbcff47a
Kcepriu <kcepriu@gmail.com>
2023-06-02 21:54:48 +0300

    add generate test data, filter for get task

--- services/tasks/serviceChangeCategoryTasks.js
+++ services/tasks/serviceChangeCategoryTasks.js
@@ -3,7 +3,10 @@ const { modelTask } = require("../../models/task");
 const { HttpError } = require("../../helpers");
 
 const serviceChangeCategoryTasks = async (id, data) => {
-  const task = await modelTask.findByIdAndUpdate(id, data, { new: true });
+  const task = await modelTask.findByIdAndUpdate(id, data, {
+    new: true,
+    select: "-createdAt -updatedAt -__v",
+  });
 
   if (!task) {
     throw HttpError(400, "Unable to find Task");
