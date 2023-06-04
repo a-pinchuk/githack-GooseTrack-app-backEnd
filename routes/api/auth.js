191e86a9b30695e69d82b7fd37b049d8aae02d44
a.pinchuk <co.born2code@gmail.com>
2023-06-04 13:27:26 +0200

    new updateavatar

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,6 +12,12 @@ router.post('/register', validateBody(schemas.registerSchema), ctrl.register);
 router.post('/login', validateBody(schemas.loginSchema), ctrl.login);
 router.post('/logout', auth, ctrl.logout);
 router.patch('/avatars', auth, upload.single('avatar'), ctrl.updateAvatar);
-router.patch('/user', auth, validateBody(schemas.updateUserSchema), ctrl.updateUser);
+router.patch(
+  '/user',
+  auth,
+  upload.single('avatar'),
+  validateBody(schemas.updateUserSchema),
+  ctrl.updateUser
+);
 
 module.exports = router;
