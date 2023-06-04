550dc9dce0b04dd0efeb7907e7c15a2a3a7f3bea
Andrew Miller <ammoldervn@gmail.com>
2023-06-04 02:05:11 -0700

    add change avatarUrl

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,34 +1,15 @@
-// const { User } = require("../../models");
+const { User } = require("../../models");
 
 const updateAvatar = async (req, res) => {
-  console.log("req.file.path :", req.file.path);
-
-  // const owner = req.user.id;
-  // const petData = req.body;
-  // const data = !req.file
-  //   ? { avatarURL: req.file.path, owner, ...petData }
-  //   : { owner, ...petData };
+  const owner = req.user.id;
   const avatarURL = req.file.path;
+  const user = await User.findByIdAndUpdate(
+    owner,
+    { avatarUrl: avatarURL },
+    { new: true }
+  );
 
-  // Pet.create(data)
-  //   .then((pet) => {
-  //     if (pet) {
-  //       User.findByIdAndUpdate(owner, { $push: { userPets: pet._id } })
-  //         .then((user) => {
-  //           if (user) {
-  //             res.status(201).json({ success: true, pet });
-  //           }
-  //         })
-  //         .catch((err) => {
-  //           throw new Error(err);
-  //         });
-  //     }
-  //   })
-  //   .catch((err) =>
-  //     res.status(400).json({ success: false, error: err, message: err.message })
-  //   );
-
-  res.json(avatarURL);
+  res.json(user.avatarUrl);
 };
 
 module.exports = updateAvatar;
