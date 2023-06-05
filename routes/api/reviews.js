bd527156259e396ca5a4070b8ed57aad9d9ac93c
a.pinchuk <co.born2code@gmail.com>
2023-06-05 14:13:35 +0200

    fixed auth midlware

--- routes/api/reviews.js
+++ routes/api/reviews.js
@@ -1,21 +1,15 @@
-const express = require("express");
-const { validateBody, isValidId, auth } = require("../../middlewares");
-const { schemas } = require("../../models/review");
+const express = require('express');
+const { validateBody, isValidId, auth } = require('../../middlewares');
+const { schemas } = require('../../models/review');
 
-const ctrl = require("../../controllers/controlerReviews");
+const ctrl = require('../../controllers/controlerReviews');
 
 const router = express.Router();
 
-router.get("/", auth, ctrl.getReviews);
-router.get("/:id", auth, isValidId, ctrl.getReviewById);
-router.post("/", auth, validateBody(schemas.schemaAddReview), ctrl.addReview);
-router.put(
-  "/:id",
-  auth,
-  isValidId,
-  validateBody(schemas.schemaAddReview),
-  ctrl.changeReview
-);
-router.delete("/:id", auth, isValidId, ctrl.deleteReview);
+router.get('/', ctrl.getReviews);
+router.get('/:id', auth, isValidId, ctrl.getReviewById);
+router.post('/', auth, validateBody(schemas.schemaAddReview), ctrl.addReview);
+router.put('/:id', auth, isValidId, validateBody(schemas.schemaAddReview), ctrl.changeReview);
+router.delete('/:id', auth, isValidId, ctrl.deleteReview);
 
 module.exports = router;
