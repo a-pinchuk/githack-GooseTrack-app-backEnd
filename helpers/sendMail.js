40cd4ce50ffb8de28f636e4598e86aff6a65317c
a.pinchuk <co.born2code@gmail.com>
2023-06-12 21:04:55 +0200

    testing outlook

--- helpers/sendMail.js
+++ helpers/sendMail.js
@@ -1,8 +1,9 @@
-const nodemailer = require("nodemailer");
+const nodemailer = require('nodemailer');
 const { EMAIL_USER, EMAIL_PASS } = process.env;
 
 const sendMail = async ({ to, subject, html }) => {
-  const from = "sandbox.smtp.mailtrap.io";
+  // const from = "sandbox.smtp.mailtrap.io";
+  const from = 'matveigoit@outlook.com';
   const email = {
     to,
     from,
@@ -10,14 +11,24 @@ const sendMail = async ({ to, subject, html }) => {
     html,
   };
 
-  const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
+  let transport = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
+    port: 587,
+    secure: false, // true for 465, false for other ports
     auth: {
-      user: EMAIL_USER,
-      pass: EMAIL_PASS,
+      user: 'matveigoit@outlook.com', // generated ethereal user
+      pass: 'qwe123goit', // generated ethereal password
     },
   });
+
+  // const transport = nodemailer.createTransport({
+  //   host: "sandbox.smtp.mailtrap.io",
+  //   port: 2525,
+  //   auth: {
+  //     user: EMAIL_USER,
+  //     pass: EMAIL_PASS,
+  //   },
+  // });
 
   await transport.sendMail(email);
 };
